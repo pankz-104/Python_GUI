a40805b06c6f561d1191b899a438f3348a7e00d7
pankaj pandey <49112512+pankz-104@users.noreply.github.com>
2020-09-05 11:37:22 +0530

    Create gui_2.py

---
+++ gui_2.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+#root = Tk()
+#root.minsize(width = 400, height = 400) # minsize of default gui window 
+#root.maxsize(width = 900, height = 800)  # maxsize of default gui window
+#label = Label(root, text="GUI_Program")
+#label.pack()
+#root.mainloop()
+
+# setting default size of GUI
+root = Tk()
+root.geometry("400x400")
+label = Label(root, text="GUI_Program")
+label.pack()
+root.mainloop()
